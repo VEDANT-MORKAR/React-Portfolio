739d5c5ca35d3c86997e4d85fef3691c6fd19664
Vedant Morkar <morkarvedant@gmail.com>
2023-07-08 12:23:05 +0530

    resume link update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "https://drive.google.com/file/d/1H0njqRSdastHYY-N-FEuNFy47kjLJ8sY/view?usp=sharing",
+      href: "https://drive.google.com/file/d/1nMPPA_mTxDhvMc78C2vY0HdJ1NMQqoNQ/view?usp=sharing",
       style: "rounded-br-md",
       //download: true,
     },
